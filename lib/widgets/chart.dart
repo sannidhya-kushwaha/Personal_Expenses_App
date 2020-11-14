c546ae8380f9b79adec9c45d190b64b9166b2953
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-14 10:10:29 +0530

    Adding chart & looping through lists

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,37 @@
+import 'package:Personal_Expenses_app/models/transaction.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
