9e89dc1187c37b1937864b16694e336c90728101
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-14 11:11:13 +0530

    Creating bars for the chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -23,7 +23,7 @@ class Chart extends StatelessWidget {
           }
         }
         return {
-          'day': DateFormat.E().format(weekDay),
+          'day': DateFormat.E().format(weekDay).substring(0, 1),
           'amount': totalSum,
         };
       },
@@ -36,7 +36,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
