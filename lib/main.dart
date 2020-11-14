3901c739e563dc2b33f3ea5e80e98d6ecdfe3cb0
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-14 14:31:05 +0530

    Showing a DatePicker

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
+              button: TextStyle(color: Colors.white),
             ),
         appBarTheme: AppBarTheme(
           textTheme: ThemeData.light().textTheme.copyWith(
