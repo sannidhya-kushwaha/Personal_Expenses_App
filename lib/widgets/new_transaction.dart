aad0c773b90ec9a82c805387c3a28dc7d609b048
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-16 12:16:38 +0530

    Creating Custom Adaptive Widgets

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import '../widgets/adaptive_flat_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -88,22 +89,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                     ),
                   ),
-                  Platform.isIOS
-                      ? CupertinoButton(
-                          child: Text(
-                            'Choose Date',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: _presentDatePicker,
-                        )
-                      : FlatButton(
-                          textColor: Theme.of(context).primaryColor,
-                          child: Text(
-                            'Choose Date',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: _presentDatePicker,
-                        )
+                  AdaptiveFlatButton('Choose Date', _presentDatePicker),
                 ],
               ),
             ),
